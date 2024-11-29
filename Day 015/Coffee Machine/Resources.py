32d85d21bd008526ac2294d0ac02068cee7579ea
44462576+PedroLantyer@users.noreply.github.com <pedrolantyer@hotmail.com>
2024-11-29 03:12:46 -0300

    Update

--- Day 015/Coffee Machine/Resources.py
+++ Day 015/Coffee Machine/Resources.py
@@ -1,8 +1,9 @@
 from decimal import Decimal
 
-coffee: list[dict] = [{"id": 1, "flavour": "Espresso", "water": 50, "coffee": 18, "milk": 0, "price": Decimal(1.50)},
-                      {"id": 2, "flavour": "Latte", "water": 200, "coffee": 24, "milk": 150, "price": Decimal(2.50)},
-                      {"id": 3, "flavour": "Cappuccino", "water": 250, "coffee": 24, "milk": 100, "price": Decimal(3.00)}]
+coffee: list[dict] = [{"id": 1, "flavour": "Espresso", "water": 50, "coffee": 18, "milk": 0, "price": Decimal("1.50")},
+                      {"id": 2, "flavour": "Latte", "water": 200, "coffee": 24, "milk": 150, "price": Decimal("2.50")},
+                      {"id": 3, "flavour": "Cappuccino", "water": 250, "coffee": 24, "milk": 100,
+                       "price": Decimal("3.00")}]
 
 coins: list[dict] = [{"id": 1, "value": Decimal(0.01), "name": "Penny"},
                      {"id": 2, "value": Decimal(0.05), "name": "Nickel"},
